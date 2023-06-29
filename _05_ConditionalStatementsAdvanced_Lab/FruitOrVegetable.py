ffe3f1776ee24a5c5ede7ba90018fea3f05cfd4a
danovd <danovski_dimitar@abv.bg>
2023-06-29 09:40:05 +0300

    Fruit Or Vegetable

---
+++ _05_ConditionalStatementsAdvanced_Lab/FruitOrVegetable.py
@@ -0,0 +1,9 @@
+fruit = input()
+if fruit == "banana" or fruit == "kiwi" or fruit == "apple" or fruit == "cherry" or fruit == "lemon"\
+    or fruit == "grapes":
+    print("fruit")
+elif fruit == "tomato" or fruit == "cucumber" or fruit == "pepper" or fruit == "carrot":
+    print("vegetable")
+else:
+    print("unknown")
+    
