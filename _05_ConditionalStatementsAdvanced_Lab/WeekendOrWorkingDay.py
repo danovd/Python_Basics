e8f125b8c2dd742be379a2de6a03c3570a5e375d
danovd <danovski_dimitar@abv.bg>
2023-06-30 00:29:48 +0300

    Weekend Or Working Day

---
+++ _05_ConditionalStatementsAdvanced_Lab/WeekendOrWorkingDay.py
@@ -0,0 +1,7 @@
+day = input()
+if day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday":
+    print("Working day")
+elif day == "Sunday" or day == "Saturday":
+    print("Weekend")
+else:
+    print("Error")
