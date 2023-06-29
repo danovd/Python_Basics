927e0d52c45c2feb31045976476c324d922f9521
danovd <danovski_dimitar@abv.bg>
2023-06-29 09:39:01 +0300

    Cinema Ticket

---
+++ _05_ConditionalStatementsAdvanced_Lab/CinemaTicket.py
@@ -0,0 +1,8 @@
+day = input()
+if day == "Monday" or day == "Tuesday"  or day == "Friday":
+    print(12)
+elif day == "Wednesday" or day == "Thursday":
+    print(14)
+elif day == "Saturday" or day == "Sunday":
+    print(16)
+    
